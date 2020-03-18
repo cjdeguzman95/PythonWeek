ce4eb53ec051f40019309c184425f940459b3b05
CJ De Guzman <cjdeguzman0@gmail.com>
2020-03-18 07:41:34 +0000

    fourth commit

--- Games/Horoscope.py
+++ Games/Horoscope.py
@@ -5,76 +5,100 @@ time.sleep(2)
 
 name = input("First, tell me your name: ")
 month = input("What is your birth month?: ")
-day = int(input("Which date of the month is your birthday in?: "))
+day = int(input("Which day of the month is your birthday in?: "))
 
-# how to make this using def?
-if month.lower() == "december":
+
+if month.lower() == "december" or month.lower() == "dec":
     if day < 21:
         astrology = "Sagittarius"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Capricorn"
-elif month.lower() == "january":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "january" or month.lower() == "jan":
     if day < 19:
         astrology = "Capricorn"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Aquarius"
-elif month.lower() == "february":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "february" or month.lower() == "feb":
     if day < 18:
         astrology = "Aquarius"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Pisces"
-elif month.lower() == "march":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "march" or month.lower() == "mar":
     if day < 20:
         astrology = "Pisces"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Aries"
-elif month.lower() == "april":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "april" or month.lower() == "apr":
     if day < 20:
         astrology = "Aries"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Taurus"
+        print("Oh, you're a {}".format(astrology))
 elif month.lower() == "may":
     if day < 20:
         astrology = "Taurus"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Gemini"
-elif month.lower() == "june":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "june" or month.lower() == "jun":
     if day < 21:
         astrology = "Gemini"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Cancer"
-elif month.lower() == "july":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "july" or month.lower() == "jul":
     if day < 22:
         astrology = "Cancer"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Leo"
-elif month.lower() == "august":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "august" or month.lower() == "aug":
     if day < 22:
         astrology = "Leo"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Virgo"
-elif month.lower() == "september":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "september" or month.lower() == "sep":
     if day < 22:
         astrology = "Virgo"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Libra"
-elif month.lower() == "october":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "october" or month.lower() == "oct":
     if day < 22:
         astrology = "Libra"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Scorpio"
-elif month.lower() == "november":
+        print("Oh, you're a {}".format(astrology))
+elif month.lower() == "november" or month.lower() == "nov":
     if day < 22:
         astrology = "Scorpio"
+        print("Oh, you're a {}".format(astrology))
     else:
         astrology = "Sagittarius"
+        print("Oh, you're a {}".format(astrology))
 
-# how to make this using def?
+
 if astrology in ["Sagittarius", "Aquarius", "Aries", "Gemini", "Leo", "Libra"]:
     fortune = "you will receive lots of money this week"
 else:
     fortune = "you will meet someone new this week!"
 
-print("Hmmm.. your fortune for the week is...")
+print("Interesting. Hmmm.. Give me a few moments...")
 time.sleep(2)
-print("{}, the universe tells me that ".format(name), fortune)
+print("{}, the universe tells me that".format(name.capitalize()), fortune)
