6bb7543e69e3141bd4a30cd5bde2fdf25c82d179
CJ De Guzman <cjdeguzman0@gmail.com>
2020-03-21 20:06:54 +0000

    revision work

--- Revision/Revision.py
+++ Revision/Revision.py
@@ -43,3 +43,38 @@ class Animal:
 
 Animal1 = Animal("dog", "bella", 12)
 Animal2 = Animal("cat", "mimi", 6)
+
+
+# Practice 2
+
+
+class Spartan:
+    def __init__(self, name, course, week):
+        self.name = name
+        self.course = course
+        self.week = week
+
+    def greeting(self):
+        return "Hi, my name is {}".format(self.name)
+
+    def stream(self):
+        return "{} is on the {} stream".format(self.name, self.course)
+
+    def trainer(self):
+        if self.course == "data":
+            return "Isabella trains {}".format(self.name)
+        else:
+            return "Isabella does not train {}".format(self.name)
+
+    def IsTraining(self):
+        if self.week <= 10:
+            return "{} is still training".format(self.name)
+        else:
+            return "{} has graduated from Sparta Academy".format(self.name)
+
+
+x = Spartan("CJ", "data", 5)
+y = Spartan("Mike", "devops", 13)
+
+print(y.trainer(), ". Also,", y.IsTraining())
+print("{} and {}.".format(x.stream(), x.IsTraining()))
