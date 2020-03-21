6abf505ac8a28aa123416d92d0c47a1cc014e731
CJ De Guzman <cjdeguzman0@gmail.com>
2020-03-21 19:08:45 +0000

    first commit

--- 190320_PythonExercises.py
+++ 190320_PythonExercises.py
@@ -1,65 +1,65 @@
-# Exercises for 19/03/20
-
-# Exercise 1: Define mathematical functions
-
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
-
-
-print(add(2, 6))
-print(subtract(2, 6))
-print(multiply(2, 6))
-print(divide(2, 6))
-
-# Exercise 2: Create a class called 'Car'
-
-
-class Car:
-
-    car_brand = "Toyota"
-
-    def sound(self):
-        return "Vroom"
-
-
-print(Car.car_brand)
-print(type(Car))
-
-# Exercise 3: Create a class called 'Fizzbuzz'
-
-
-class Fizzbuzz:
-    def __init__(self, number):
-        self.number = number
-
-    def isFizzbuzz(self):
-        if self.number % 3 == 0 and self.number % 5 ==0:
-            print("Fizzbuzz")
-        elif self.number % 3 == 0:
-            print("Fizz")
-        elif self.number % 5 == 0:
-            print("Buzz")
-        else:
-            print(self.number)
-
-
-number_range = range(101)
-for n in number_range:
-    num1 = Fizzbuzz(n)
-    num1.isFizzbuzz()
+# # Exercises for 19/03/20
+#
+# # Exercise 1: Define mathematical functions
+#
+#
+# def add(a, b):
+#     return a + b
+#
+#
+# def subtract(a, b):
+#     return a - b
+#
+#
+# def multiply(a, b):
+#     return a * b
+#
+#
+# def divide(a, b):
+#     return a / b
+#
+#
+# print(add(2, 6))
+# print(subtract(2, 6))
+# print(multiply(2, 6))
+# print(divide(2, 6))
+#
+# # Exercise 2: Create a class called 'Car'
+#
+#
+# class Car:
+#
+#     car_brand = "Toyota"
+#
+#     def sound(self):
+#         return "Vroom"
+#
+#
+# print(Car.car_brand)
+# print(type(Car))
+#
+# # Exercise 3: Create a class called 'Fizzbuzz'
+#
+#
+# class Fizzbuzz:
+#     def __init__(self, number):
+#         self.number = number
+#
+#     def isFizzbuzz(self):
+#         if self.number % 3 == 0 and self.number % 5 ==0:
+#             print("Fizzbuzz")
+#         elif self.number % 3 == 0:
+#             print("Fizz")
+#         elif self.number % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(self.number)
+#
+#
+# number_range = range(101)
+# for n in number_range:
+#     num1 = Fizzbuzz(n)
+#     num1.isFizzbuzz()
 
 # Exercise 4: Create a class called 'Student'
 
@@ -84,6 +84,5 @@ class Student:
 student1 = Student('James', 19, False)
 student2 = Student('Sarah', 23, True)
 
-print(student1.student_age())
-print(student2.need_stay())
-
+student1.student_age()
+student2.need_stay()
