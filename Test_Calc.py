23d639988c011a3a00f3378f4abe7d428aaa357e
CJ De Guzman <cjdeguzman0@gmail.com>
2020-03-23 16:02:09 +0000

    unit testing

--- Test_Calc.py
+++ Test_Calc.py
@@ -4,19 +4,19 @@ from Calculator import Simplecalc
 
 class Testcalc(unittest.TestCase):
 
-    x = Simplecalc()
+    calc = Simplecalc()
 
     def test_add(self):
-        self.assertEqual(self.x.add(10, 5), 15)
+        self.assertEqual(self.calc.add(10, 5), 15)
 
     def test_subtract(self):
-        self.assertEqual(self.x.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_multiply(self):
-        self.assertEqual(self.x.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
 
     def test_divide(self):
-        self.assertEqual(self.x.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
 
 if __name__ == "__main__":
